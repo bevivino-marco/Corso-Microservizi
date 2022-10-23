e6286d19f43b970337eb9ba26b1b31d1bea8c3d5
m.bevivino <{m.bevivino@reply.it}>
2022-10-23 15:28:05 +0200

    v2 completata

--- currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/in28minutes/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.microservice.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +13,8 @@ import java.util.HashMap;
 @RestController
 public class CurrencyConversionController {
 
-
+    @Autowired
+    private CurrencyExchangeProxy proxy;
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(
@@ -34,6 +36,25 @@ public class CurrencyConversionController {
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
                 currencyConversion.getEnvironment()+ " " + "rest template");
+
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+
+
+        CurrencyConversion currencyConversion =proxy.retrieveExchangeValue(from,to);
+        currencyConversion.setCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+        return currencyConversion;
+//        return new CurrencyConversion(currencyConversion.getId(),
+//                from, to, quantity,
+//                currencyConversion.getConversionMultiple(),
+//                quantity.multiply(currencyConversion.getConversionMultiple()),
+//                currencyConversion.getEnvironment()+ " " + "feign template");
 
     }
 }
